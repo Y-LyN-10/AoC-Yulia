bb7c011534f0420de274e60041d9ba6bebf56a1b
Yulia Tenincheva <yulia.tenincheva@mentormate.com>
2022-12-02 09:59:06 +0200

    Added solution for part 2 of the daily task

--- 02-rock-paper-scissors/index.js
+++ 02-rock-paper-scissors/index.js
@@ -10,16 +10,59 @@ module.exports = async function (rl) {
   let score = 0;
   const pointsMap = { 'X': 1, 'Y': 2, 'Z': 3};
 
+  // Solution for Part 1 of the task
+
+  // for await (const line of rl) {
+  //   let [opponent, me] = line.split(' ');
+  //
+  //   if(isWin(opponent, me)){
+  //       score += 6;
+  //   } else if (isDraw(opponent, me)) {
+  //       score += 3;
+  //   } // else stays 0
+  //
+  //   score += pointsMap[me];
+  // }
+  //
+  // console.log(score);
+
+  // part 2 is to use a completely different strategy (rules)
+  // X = need to lose, Y = need to end the round in draw and Z = need to win
+  // the output is calculated the same way
+
+  let rules = {
+    'win': {
+      'C': 'X',
+      'A': 'Y',
+      'B': 'Z'
+    },
+    'draw': {
+      'A': 'X',
+      'B': 'Y',
+      'C': 'Z'
+    },
+    'lose': {
+      'A': 'Z',
+      'B': 'X',
+      'C': 'Y'
+    }
+  };
+
   for await (const line of rl) {
     let [opponent, me] = line.split(' ');
+    let shapeToUse;
 
-    if(isWin(opponent, me)){
+    if(me === 'Z'){
         score += 6;
-    } else if (isDraw(opponent, me)) {
+        shapeToUse = rules['win'][opponent];
+    } else if (me === 'Y') {
         score += 3;
-    } // else stays 0
+        shapeToUse = rules['draw'][opponent];
+    } else {
+        shapeToUse = rules['lose'][opponent];
+    }
 
-    score += pointsMap[me];
+    score += pointsMap[shapeToUse];
   }
 
   console.log(score);
